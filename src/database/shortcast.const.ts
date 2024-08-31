04715149ffbb5d66a88c842dbb3757b3d8ea06c5
Danniel Navas <dannielnavas@gmail.com>
2024-08-31 18:47:37 -0500

    💚  chore: Update vercel.json configuration to use dist/main.ts for Vercel deployment

--- src/database/shortcast.const.ts
+++ src/database/shortcast.const.ts
@@ -3,30 +3,36 @@ export const SHORTCAST = [
     title: 'Shortcast 06 - ¿Donde estaba TNG durante la Guerra del Dominio?',
     name: '06',
     thumbnail: 'https://i.ytimg.com/vi_webp/xRboKTxKBks/sddefault.webp',
+    song: 'https://firebasestorage.googleapis.com/v0/b/lavaderosonico.appspot.com/o/06.mp3?alt=media&token=',
   },
   {
     title: 'Shortcast 05 - Primeros oficiales. Cual es el mejor?',
     name: '05',
     thumbnail: 'https://i.ytimg.com/vi_webp/nCKx2NkWvuU/sddefault.webp',
+    song: 'https://firebasestorage.googleapis.com/v0/b/lavaderosonico.appspot.com/o/05.mp3?alt=media&token=',
   },
   {
     title: 'Shortcast 04 - La Voyager y los maquis...',
     name: '04',
     thumbnail: 'https://i.ytimg.com/vi_webp/qL7biUhYTKw/maxresdefault.webp',
+    song: 'https://firebasestorage.googleapis.com/v0/b/lavaderosonico.appspot.com/o/04.mp3?alt=media&token=',
   },
   {
     title: 'Shortcast 03 - Worf, el peor padre?',
     name: '03',
     thumbnail: 'https://i.ytimg.com/vi_webp/yuP2i1wxhag/maxresdefault.webp',
+    song: 'https://firebasestorage.googleapis.com/v0/b/lavaderosonico.appspot.com/o/03.mp3?alt=media&token=',
   },
   {
     title: 'Shortcast 02 - Sistemas de aterrizaje planetario',
     name: '02',
     thumbnail: 'https://i.ytimg.com/vi_webp/5PBnXT6LpJM/maxresdefault.webp',
+    song: 'https://firebasestorage.googleapis.com/v0/b/lavaderosonico.appspot.com/o/02.mp3?alt=media&token=',
   },
   {
     title: 'Shortcast 01 - Crossovers',
     name: '01',
     thumbnail: 'https://i.ytimg.com/vi_webp/s1wwkkXPbxs/sddefault.webp',
+    song: 'https://firebasestorage.googleapis.com/v0/b/lavaderosonico.appspot.com/o/01.mp3?alt=media&token=',
   },
 ];
